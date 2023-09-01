ac57fd4ba0bb30c39a11101a825578647c1de549
Bruno Luiz de Deus Adão <brunoblda@gmail.com>
2023-08-31 22:52:07 -0300

    fix: slice da matricula

--- logical_layer/automation.py
+++ logical_layer/automation.py
@@ -4,12 +4,16 @@ import pyautogui
 import requests
 from utils_layer import utils
 
+
 class DeclaracaoCtcAutomate:
     """Classe responsavel pela automacao"""
+
     def __init__(self, dict_person, mouse_coordenates) -> None:
         self.mouse_coordenates = mouse_coordenates
         self.dict_person = dict_person
-        self.coordenates_mouse_list = utils.mouse_coordenates_to_list(self.mouse_coordenates)
+        self.coordenates_mouse_list = utils.mouse_coordenates_to_list(
+            self.mouse_coordenates
+        )
 
     def printar(self):
         """Printa as variaveis iniciais"""
@@ -18,16 +22,21 @@ class DeclaracaoCtcAutomate:
 
     def execute_substitution(self, index, frase):
         """Realiza a substituicao dos campos"""
-        pyautogui.click(self.coordenates_mouse_list[index][0],self.coordenates_mouse_list[index][1])
+        pyautogui.click(
+            self.coordenates_mouse_list[index][0], self.coordenates_mouse_list[index][1]
+        )
         pyautogui.press("enter")
-        pyautogui.tripleClick(self.coordenates_mouse_list[index][0],
-                              self.coordenates_mouse_list[index][1])
+        pyautogui.tripleClick(
+            self.coordenates_mouse_list[index][0], self.coordenates_mouse_list[index][1]
+        )
         pyautogui.write(frase)
 
     def execute_automate(self):
         """Executa a automacao na declaracao no SEI"""
         try:
-            self.dict_person["END RESID-UF"] = utils.get_uf_api(self.dict_person['END RESID-CEP'])
+            self.dict_person["END RESID-UF"] = utils.get_uf_api(
+                self.dict_person["END RESID-CEP"]
+            )
         except requests.exceptions.Timeout:
             self.dict_person["END RESID-UF"] = "NF"
         except KeyError:
@@ -36,16 +45,20 @@ class DeclaracaoCtcAutomate:
             self.dict_person["END RESID-UF"] = "NF"
 
         date_orgao_spub_equal = utils.compare_date_orgao_spub(
-            self.dict_person["DIA OCOR INGR ÓRGÃO EV"], self.dict_person["MÊS ING SPUB"])
+            self.dict_person["DIA OCOR INGR ÓRGÃO EV"], self.dict_person["MÊS ING SPUB"]
+        )
         if date_orgao_spub_equal:
             self.dict_person["DIA OCOR INGR ÓRGÃO EV"] = utils.date_mask(
-                self.dict_person["DIA OCOR INGR ÓRGÃO EV"])
+                self.dict_person["DIA OCOR INGR ÓRGÃO EV"]
+            )
         else:
             self.dict_person["DIA OCOR INGR ÓRGÃO EV"] = "XX/XX/XXXX"
         time.sleep(5)
         index_coordenates_mouse_initial = 1
         pyautogui.PAUSE = 1
-        pyautogui.click(self.coordenates_mouse_list[0][0],self.coordenates_mouse_list[0][1])
+        pyautogui.click(
+            self.coordenates_mouse_list[0][0], self.coordenates_mouse_list[0][1]
+        )
         frases = [
             f"NOME DO(A) SERVIDOR(A): {self.dict_person['NOME SERVIDOR']}",
             self.dict_person["IDENTIDADE-NÚMERO"],
@@ -54,7 +67,7 @@ class DeclaracaoCtcAutomate:
             self.dict_person["IDENTIDADE-DATA EXP"],
             utils.cpf_mask(self.dict_person["CPF SERVIDOR"]),
             utils.eleitor_mask(self.dict_person["TÍTULO ELEITOR"]),
-            self.dict_person["VÍNCULO SERVIDOR"][7:],
+            self.dict_person["VÍNCULO SERVIDOR"][6:],
             utils.date_mask(self.dict_person["DATA NASCIMENTO SERVIDOR"]),
             self.dict_person["NOME MÃE"],
             f"{self.dict_person['END RESID-LOGRADOURO']} NUMERO: "
@@ -68,7 +81,7 @@ class DeclaracaoCtcAutomate:
             f"Data de Ingresso no servico publico (sem interrupcao): "
             f"{self.dict_person['DIA OCOR INGR ÓRGÃO EV']}",
             f"Data da Aposentadoria: {utils.date_mask(self.dict_person['DIA APOSENTADORIA'])}",
-            f"NIT: {utils.nit_mask(self.dict_person['PIS PASEP'])}"
+            f"NIT: {utils.nit_mask(self.dict_person['PIS PASEP'])}",
         ]
         for i, frase in enumerate(frases):
-            self.execute_substitution(i+index_coordenates_mouse_initial,frase)
+            self.execute_substitution(i + index_coordenates_mouse_initial, frase)
